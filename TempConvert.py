516325db610c41266ecdefa84870248410396fe5
mtupa2 <17500380@nebraska.edu>
2025-09-14 09:34:16 -0500

    Working TempConvert.py

--- TempConvert.py
+++ TempConvert.py
@@ -6,12 +6,19 @@
 
 def main():
   #Prompt the user for a Fahrenheit temperature
+  TEMPF=input("Enter Value:")
+  number_float = float(TEMPF)
+  rounded_number = round(number_float, 1)
+  print(f"The number rounded to one decimal place is: {rounded_number}")
+  print("Invalid input. Please enter a valid number.")
   #Convert that temperature to celsius, rounding to 1 decimal percision
+  celsius = (fahrenheit - 32) * 5/9
+
   #Output converted temperature.
-  tempF = 80
+  
 
-  tempC = tempF / 2
+  
 
-  print(tempF, "is ", tempC, "degrees celsius.")
+  print(rounded_number, "is ", celsius, "degrees celsius.")
 if __name__ == '__main__':
   main()
